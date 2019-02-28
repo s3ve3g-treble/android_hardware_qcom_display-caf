0beefaa291e5e820a645ebdfff9b0d2ad48e942b
PythonLimited <Leonardomeitz@gmail.com>
2019-02-28 14:22:11 +0100

    display: import

---
+++ libhwcomposer/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/../common.mk
+include $(CLEAR_VARS)
+
+LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_RELATIVE_PATH    := hw
+LOCAL_VENDOR_MODULE           := true
+LOCAL_MODULE_TAGS             := optional
+LOCAL_HEADER_LIBRARIES	      := libui_headers
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes) \
+                                 frameworks/native/libs/arect/include
+
+ifeq ($(strip $(TARGET_USES_QCOM_DISPLAY_PP)),true)
+LOCAL_C_INCLUDES              += $(TARGET_OUT_HEADERS)/qdcm/inc \
+                                 $(TARGET_OUT_HEADERS)/common/inc \
+                                 $(TARGET_OUT_HEADERS)/pp/inc
+endif
+
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
+                                 libhdmi libqdutils libhardware_legacy \
+                                 libdl libmemalloc libqservice libsync \
+                                 libbinder libvirtual \
+                                 libbfqio_vendor
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
+#Enable Dynamic FPS if PHASE_OFFSET is not set
+ifeq ($(VSYNC_EVENT_PHASE_OFFSET_NS),)
+    LOCAL_CFLAGS += -DDYNAMIC_FPS
+endif
+ifeq ($(GET_DISPLAY_SECURE_STATUS_FROM_HWC),true)
+    LOCAL_CFLAGS += -DGET_DISPLAY_SECURE_STATUS_FROM_HWC
+endif
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_SRC_FILES               := hwc.cpp          \
+                                 hwc_utils.cpp    \
+                                 hwc_uevents.cpp  \
+                                 hwc_vsync.cpp    \
+                                 hwc_fbupdate.cpp \
+                                 hwc_mdpcomp.cpp  \
+                                 hwc_copybit.cpp  \
+                                 hwc_qclient.cpp  \
+                                 hwc_dump_layers.cpp \
+                                 hwc_ad.cpp \
+                                 hwc_virtual.cpp
+
+ifeq ($(call is-board-platform-in-list, mpq8092 msm_bronze msm8916), true)
+    LOCAL_SRC_FILES += hwc_vpuclient.cpp
+endif
+
+
+include $(BUILD_SHARED_LIBRARY)
